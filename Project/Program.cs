0e137fe64c98bf41feb9b69f73e6679619c92329
Dusan Zurkovic <d.zurkovic@vega.local>
2021-11-21 15:09:34 +0100

    Initial commit

---
+++ Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Peoject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!213");
+        }
+    }
+}
